40e68cd9548cb2ff38fd2b339eaa0187d5d34fff
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-07-12 21:40:05 +0300

    feat: add service file

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.render('project');
+	res.render('learning');
 });
 
 app.listen(3000, function () {
