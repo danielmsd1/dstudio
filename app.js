8b95d1437d6ba91befe25b26aec6e065d041cb28
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-07-12 13:05:47 +0300

    feat: add projectsview file

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.render('home');
+	res.render('projectsview');
 });
 
 app.listen(3000, function () {
