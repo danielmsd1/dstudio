cebe761f3ffdbfa59d200dac02d8bc66927539d6
Daniel Simiyu <danielmsimiyu71@gmail.com>
2022-07-13 21:30:08 +0300

    feat: add careersview page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.render('team');
+	res.render('careersview');
 });
 
 app.listen(3000, function () {
